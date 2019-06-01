295a060e5c5a8771d122ebd675966acc3a37f63c
esme <esmelingyr@gmail.com>
2019-06-01 10:33:55 -0700

    finished basics

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -11,33 +11,53 @@ var BinarySearchTree = function(value) {
 var binaryTreeMethods = {};
 
 binaryTreeMethods.insert = function(value) {
-    // check if value is left or right of parent node
-    // make sure that there is 1 or less child of each node
-    if (this.left === null && value < this.value) {
-        var leftTree = BinarySearchTree(value);
-        this.left = leftTree;
-    } else if (this.right === null && value > this.value) {
-        var rightTree = BinarySearchTree(value);
-        this.right = rightTree;
-    } else if (this.left !== null) {
-        this.left.insert(value);
-    } else if (this.right !== null) {
-        this.right.insert(value);
+    if (value < this.value) {
+        if (this.left === null) {
+            var leftTree = BinarySearchTree(value);
+            this.left = leftTree;
+        } else {
+            this.left.insert(value);
+        }
     }
 
+    if (value > this.value) {
+        if (this.right === null) {
+            var rightTree = BinarySearchTree(value);
+            this.right = rightTree;
+        } else {
+            this.right.insert(value);
+        }
+    }
 }
 
 
 binaryTreeMethods.contains = function(value) {
-
+    if (this.value === value) {
+        return true;
+    }
+    if (value < this.value) {
+        if (this.left === null) {return false};
+        return this.left.contains(value);
+    } else if (value > this.value) {
+        if (this.right === null) {return false};
+        return this.right.contains(value);
+    }
+    return false;
 }
 
 
-binaryTreeMethods.depthFirstLog = function() {
-
+binaryTreeMethods.depthFirstLog = function(func) {
+    func(this.value);
+    if (this.left !== null) {
+        this.left.depthFirstLog(func);
+    }
+    if (this.right !== null) {
+        this.right.depthFirstLog(func);
+    }
 }
 /*
  * Complexity: What is the time complexity of the above functions?
+ * log n
  */
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,30 +1,62 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = []; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  var index = this._storage.indexOf(item);
-  if(index === -1) {
-    this._storage.push(item);
+  if (this._storage[item] === undefined) {
+    this._storage[item] = item;
   }
 };
 
 setPrototype.contains = function(item) {
-  var index = this._storage.indexOf(item);
-  if(index !== -1) {
+  if (this._storage[item] !== undefined) {
     return true;
-  } else return false;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
-  var index = this._storage.indexOf(item);
-  delete this._storage[index];
+  if (this._storage[item] !== undefined) {
+    delete this._storage[item];
+  }
 };
+
+
+
+
+// var Set = function() {
+//   var set = Object.create(setPrototype);
+//   set._storage = []; // fix me
+//   return set;
+// };
+
+// var setPrototype = {};
+
+// setPrototype.add = function(item) {
+//   var index = this._storage.indexOf(item);
+//   if(index === -1) {
+//     this._storage.push(item);
+//   }
+// };
+
+// setPrototype.contains = function(item) {
+//   var index = this._storage.indexOf(item);
+//   if(index !== -1) {
+//     return true;
+//   } else return false;
+// };
+
+// setPrototype.remove = function(item) {
+//   var index = this._storage.indexOf(item);
+//   delete this._storage[index];
+// };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * constant
  */
+
